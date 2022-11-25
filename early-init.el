bf7976597183bc20453e38f396539985542999fd
Jousimies <duan_n@outlook.com>
2022-11-25 11:47:10 +0800

    start emacs with maximized frame.

--- early-init.el
+++ early-init.el
@@ -16,6 +16,8 @@
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars . 0) default-frame-alist)
 (push '(undecorated . t) default-frame-alist)
+
+(push '(fullscreen . maximized) initial-frame-alist)
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
 ;; larger than the system default.
