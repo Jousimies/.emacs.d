afc7048e41af2afa418489027d7f942c39332845
Jousimies <duan_n@outlook.com>
2023-03-08 00:55:10 +0800

    auto change theme with system.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -55,6 +55,14 @@
 
 (load-theme 'modus-operandi t)
 
+(defun my/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light (load-theme 'modus-operandi t))
+    ('dark (load-theme 'modus-vivendi t))))
+(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
+
 (define-fringe-bitmap 'right-curly-arrow  [])
 (define-fringe-bitmap 'left-curly-arrow  [])
 
