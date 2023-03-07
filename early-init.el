3b30d26a8fe18584cf46351e02be67994278549f
Jousimies <duan_n@outlook.com>
2023-03-07 18:33:28 +0800

    untrack emacs.org, instead of .el file.

---
+++ early-init.el
@@ -0,0 +1,35 @@
+;;; early-init.el --- Early Init File -*- lexical-binding: t; no-byte-compile: t -*-
+
+;; Profiling since here when in debug-mode
+(when init-file-debug
+  (profiler-start 'cpu)
+  (add-hook 'window-setup-hook #'profiler-stop 0))
+
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 1.0)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars . 0) default-frame-alist)
+(push '(undecorated . t) default-frame-alist)
+(push '(fullscreen . maximized) initial-frame-alist)
+
+;; To suppress flashing at startup
+(setq-default inhibit-redisplay t
+              inhibit-message t)
+
+(add-hook 'after-init-hook
+          (lambda ()
+            (setq-default inhibit-redisplay nil
+                          inhibit-message nil)
+            (redisplay)))
+
+;; Disable mode-line when emacs startup.
+(setq-default mode-line-format nil)
+
+(setq byte-compile-warnings nil)
+
+(fset 'display-startup-echo-area-message 'ignore)
+;;; early-init.el ends here
