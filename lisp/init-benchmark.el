6443317c40efd252645cce2e864a4e9d40ff3f53
Jousimies <duan_n@outlook.com>
2023-03-08 00:36:17 +0800

    add link.

--- lisp/init-benchmark.el
+++ lisp/init-benchmark.el
@@ -4,6 +4,7 @@
 
 ;;; Code:
 
+;; https://github.com/purcell/emacs.d/blob/master/lisp/init-benchmarking.el
 (defun sanityinc/time-subtract-millis (b a)
   (* 1000.0 (float-time (time-subtract b a))))
 
