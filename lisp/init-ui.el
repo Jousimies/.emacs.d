534942fda88ee903870f6a8dd7fe76faf52769b5
Jousimies <duan_n@outlook.com>
2023-03-10 23:12:50 +0800

    format.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -16,44 +16,42 @@
   (setq modus-themes-italic-constructs t)
 
   (setq modus-themes-common-palette-overrides
-    '(;; mode-line
-      (border-mode-line-active unspecified)
-      (border-mode-line-inactive unspecified)
-      (bg-mode-line-active bg-main)
-      (fg-mode-line-active fg-main)
+        '(;; mode-line
+          (border-mode-line-active unspecified)
+          (border-mode-line-inactive unspecified)
+          (bg-mode-line-active bg-main)
+          (fg-mode-line-active fg-main)
 
-      ;; line-number
-      (fg-line-number-inactive "gray50")
-      (fg-line-number-active red-cooler)
-      (bg-line-number-inactive unspecified)
-      (bg-line-number-active unspecified)
-      ;; link
-      (underline-link border)
-      (underline-link-visited border)
-      (underline-link-symbolic border)
+          ;; line-number
+          (fg-line-number-inactive "gray50")
+          (fg-line-number-active red-cooler)
+          (bg-line-number-inactive unspecified)
+          (bg-line-number-active unspecified)
+          ;; link
+          (underline-link border)
+          (underline-link-visited border)
+          (underline-link-symbolic border)
 
-      ;; org agenda
-      (date-common cyan)   ; default value (for timestamps and more)
-      (date-deadline red-warmer)
-      (date-event magenta-warmer)
-      (date-holiday blue) ; for M-x calendar
-      (date-now yellow-warmer)
-      (date-scheduled magenta-cooler)
-      (date-weekday cyan-cooler)
-      (date-weekend blue-faint)
+          ;; org agenda
+          (date-common cyan)   ; default value (for timestamps and more)
+          (date-deadline red-warmer)
+          (date-event magenta-warmer)
+          (date-holiday blue) ; for M-x calendar
+          (date-now yellow-warmer)
+          (date-scheduled magenta-cooler)
+          (date-weekday cyan-cooler)
+          (date-weekend blue-faint)
 
-      ;; org heading
-      (fg-heading-1 blue-warmer)
-      (fg-heading-2 yellow-cooler)
-      (fg-heading-3 cyan-cooler)))
+          ;; org heading
+          (fg-heading-1 blue-warmer)
+          (fg-heading-2 yellow-cooler)
+          (fg-heading-3 cyan-cooler)))
 
   (setq modus-themes-prompts '(extrabold italic))
 
   (setq modus-themes-completions
-    '((matches . (extrabold))
-      (selection . (semibold italic text-also)))))
-
-(load-theme 'modus-operandi t)
+        '((matches . (extrabold))
+          (selection . (semibold italic text-also)))))
 
 (defun my/apply-theme (appearance)
   "Load theme, taking current system APPEARANCE into consideration."
