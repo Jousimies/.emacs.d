b865f89507eea375905073f2e7bcf600b12fbf14
Jousimies <duan_n@outlook.com>
2023-03-23 16:17:49 +0800

    telega: unbind keys.

--- lisp/init-telega.el
+++ lisp/init-telega.el
@@ -14,7 +14,11 @@
                                        (select . t))))
 
 (evil-define-key 'normal telega-root-mode-map
-    "gs" nil)
+  "gs" nil
+  "q" nil)
+
+(evil-define-key 'normal telega-msg-button-map
+  "SPC" nil)
 
 (use-package telega-notifications
   :hook (telega-load . telega-notifications-mode))
