31e49506a559c0e0d0d4ea880f1c799e527c5636
Jousimies <duan_n@outlook.com>
2023-03-26 12:19:08 +0800

    emms: make it better use.

--- lisp/init-emms.el
+++ lisp/init-emms.el
@@ -1,6 +1,26 @@
+;;;###autoload
+(defun my/music ()
+    (interactive)
+    (emms-browser)
+    (emms-playlist-mode-go))
+
+;;;###autoload
+(defun my/toggle-emms (arg)
+    "Toggle Emms playback: start if stopped, stop if playing.
+With universal argument (C-u), pause playback instead of stopping."
+    (interactive "P")
+    (if (get-buffer "*Music*")
+        (if emms-player-playing-p
+            (if arg
+                (emms-stop)
+              (emms-pause))
+          (emms-start))
+      (my/music)))
+
 (use-package emms
-  :bind (("<f5>" . emms)
-         ("C-<f5>" . my/toggle-emms))
+  :bind (("C-<f8>" . my/toggle-emms)
+         ("C-<f9>" . emms-seek-forward)
+         ("C-<f7>" . emms-seek-backward))
   :config
   (setq emms-directory (expand-file-name "cache/emms" user-emacs-directory))
   (setq emms-player-list '(emms-player-mpv))
@@ -14,21 +34,18 @@
                                         (select . t)
                                         (mode-line-format . none))))
   (evil-define-key 'normal emms-playlist-mode-map
-    "q" 'quit-window)
-
-  (defun my/toggle-emms (arg)
-    "Toggle Emms playback: start if stopped, stop if playing.
-With universal argument (C-u), pause playback instead of stopping."
-    (interactive "P")
-    (if emms-player-playing-p
-        (if arg
-            (emms-stop)
-          (emms-pause))
-      (emms-start))))
+    "q" 'quit-window))
 
 (use-package emms-playlist-mode
-  :after emms
+  :commands emms-playlist-mode-go
+  :general (my/space-leader-def
+             "bp" '(my/switch-to-music :wk "Music"))
   :config
+  (defun my/switch-to-music ()
+    (interactive)
+    (if (get-buffer "*Music*")
+        (emms-playlist-mode-go)
+      (my/music)))
   (setq emms-playlist-mode-center-when-go t))
 
 (use-package emms-source-file
@@ -49,13 +66,14 @@ With universal argument (C-u), pause playback instead of stopping."
   (emms-all))
 
 (use-package emms-browser
-  :after emms
+  :commands emms-browser
   :config
   (setq emms-browser-thumbnail-small-size 64)
   (setq emms-browser-thumbnail-medium-size 128)
 
   (evil-define-key 'normal emms-browser-mode-map
-    "gb" nil))
+    "gb" nil
+    "q" 'quit-window))
 
 (use-package emms-lyrics
   :hook (emms-player-started-hook . emms-lyrics-toggle-display-on-modeline))
