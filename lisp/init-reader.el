42a5f6bcda5eaa1249d0b7e274ce31eff528afde
Jousimies <duan_n@outlook.com>
2023-04-25 16:08:59 +0800

    reuse pdf-tools.
    原本使用的 fast-view-mode 有点问题，更新后不再需要使用 eaf 了。

--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -4,9 +4,76 @@
 
 ;;; Code:
 
-(require 'eaf)
-(setq eaf-config-location (expand-file-name "cache/eaf" user-emacs-directory))
-(require 'eaf-pdf-viewer)
+(use-package pdf-tools
+  :load-path "~/.emacs.d/packages/pdf-tools/"
+  :mode ("\\.[pP][dD][fF]\\'" . pdf-view-mode)
+  :magic ("%PDF" . pdf-view-mode)
+  :config
+  (pdf-tools-install t nil t nil))
+
+(use-package pdf-view
+  :hook ((pdf-tools-enabled . pdf-view-themed-minor-mode))
+  :init
+  (add-to-list 'display-buffer-alist '("\\.pdf\\'"
+                                         (display-buffer-in-tab)
+                                         (tab-name . "PDF") (tab-group . "PDF")))
+  :config
+  (setq pdf-view-display-size 'fit-width)
+  (setq pdf-view-use-unicode-ligther nil)
+  (setq pdf-view-use-scaling t)
+  (setq pdf-view-use-imagemagick nil)
+  (setq pdf-annot-activate-created-annotations nil))
+
+(use-package pdf-occur
+  :hook (pdf-view-mode . pdf-occur-global-minor-mode))
+
+(use-package pdf-history
+  :hook (pdf-view-mode . pdf-history-minor-mode))
+
+(use-package pdf-links
+  :hook (pdf-view-mode . pdf-links-minor-mode))
+
+(use-package pdf-outline
+  :hook (pdf-view-mode . pdf-outline-minor-mode)
+  :bind (:map pdf-outline-buffer-mode-map
+              ("RET" . pdf-outline-follow-link-and-quit)))
+
+(use-package pdf-annot
+  :hook (pdf-view-mode . pdf-annot-minor-mode)
+  :bind (:map pdf-annot-edit-contents-minor-mode-map
+              ("<return>" . pdf-annot-edit-contents-commit)
+              ("<S-return>" . newline))
+  :config
+  (evil-declare-key 'normal pdf-view-mode-map
+    "gh" 'pdf-annot-add-highlight-markup-annotation
+    "ga" 'pdf-annot-add-text-annotation
+    "gd" 'pdf-annot-delete))
+
+(use-package pdf-sync
+  :hook (pdf-view-mode . pdf-sync-minor-mode))
+
+(use-package pdf-cache
+  :after pdf-view
+  :config
+  (define-pdf-cache-function pagelabels))
+
+(use-package pdf-misc
+  :after pdf-view
+  :config
+  (setq pdf-misc-print-program-executable "/usr/bin/lp")
+  (defun mrb/pdf-misc-print-pages(filename pages &optional interactive-p)
+    "Wrapper for `pdf-misc-print-document` to add page selection support."
+    (interactive (list (pdf-view-buffer-file-name)
+                       (read-string "Page range (empty for all pages): "
+                                    (number-to-string (pdf-view-current-page)))
+                       t) pdf-view-mode)
+    (let ((pdf-misc-print-program-args
+           (if (not (string-blank-p pages))
+               (cons (concat "-P " pages) pdf-misc-print-program-args)
+             pdf-misc-print-program-args)))
+      (pdf-misc-print-document filename)))
+  (with-eval-after-load 'pdf-view
+    (define-key pdf-view-mode-map [remap pdf-misc-print-document] #'mrb/pdf-misc-print-pages)))
 
 (use-package nov
   :mode (".epub" . nov-mode)
