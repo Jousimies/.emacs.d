788c0b7098ae6b3c0a77fff64724fec924832e36
Jousimies <duan_n@outlook.com>
2023-06-04 22:57:52 +0800

    feat(fullscreen)

--- early-init.el
+++ early-init.el
@@ -17,19 +17,20 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars . 0) default-frame-alist)
-(push '(undecorated . t) default-frame-alist)
+;; (push '(undecorated . t) default-frame-alist)
 (push '(width . 100) default-frame-alist)
-(push '(fullscreen . maximized) initial-frame-alist)
+(push '(fullscreen . fullscreen) initial-frame-alist)
+;; (push '(fullscreen . maximized) initial-frame-alist)
 
 ;; To suppress flashing at startup
-(setq-default inhibit-redisplay t
-              inhibit-message t)
+;; (setq-default inhibit-redisplay t
+;;               inhibit-message t)
 
-(add-hook 'window-setup-hook
-          (lambda ()
-            (setq-default inhibit-redisplay nil
-                          inhibit-message nil)
-            (redisplay)))
+;; (add-hook 'window-setup-hook
+;;           (lambda ()
+;;             (setq-default inhibit-redisplay nil
+;;                           inhibit-message nil)
+;;             (redisplay)))
 
 ;; Disable mode-line when emacs startup.
 (setq-default mode-line-format nil)
