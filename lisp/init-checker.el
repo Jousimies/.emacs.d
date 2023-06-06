3a10785041e16b096bcf3d89624e6ed4a3a589b1
Jousimies <duan_n@outlook.com>
2023-06-06 09:09:07 +0800

    feat(spell): use jinx replace of ispell and flyspell.

--- lisp/init-checker.el
+++ lisp/init-checker.el
@@ -4,31 +4,13 @@
 
 ;;; Code:
 
-(use-package ispell
-  :defer t
+(use-package jinx
+  :hook (org-mode . jinx-mode)
+  :bind (("M-$" . jinx-correct)
+         ("C-M-$" . jinx-languages))
   :config
-  (setq ispell-program-name "/opt/homebrew/bin/aspell")
-  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together"))
-  (setq ispell-aspell-dict-dir
-        (ispell-get-aspell-config-value "dict-dir"))
-
-  (setq ispell-aspell-data-dir
-        (ispell-get-aspell-config-value "data-dir"))
-
-  (setq ispell-personal-dictionary (expand-file-name "config/ispell/.aspell.en.pws" my-galaxy))
-
-  (setq-default ispell-following-word t
-                ispell-quietly t))
-
-(use-package flyspell
-  :hook (org-mode . flyspell-mode))
-
-(use-package flyspell-correct
-  :commands flyspell-correct-wrapper
-  :bind ([remap flyspell-auto-correct-previous-word] . flyspell-correct-wrapper))
-
-(use-package flymake
-  :hook (prog-mode . flymake-mode))
+  (setq jinx-languages '("en" "fr" "de"))
+  (add-to-list 'jinx-exclude-regexps '(t "\\cc")))
 
 (provide 'init-checker)
 ;;; init-checker.el ends here.
