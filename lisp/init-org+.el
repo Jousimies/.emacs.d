277dac29f146fa3b58c95da031080eee95c6574e
Jousimies <duan_n@outlook.com>
2023-10-05 12:14:34 +0800

    feature: hammy, timer for Emacs.

--- lisp/init-org+.el
+++ lisp/init-org+.el
@@ -158,5 +158,40 @@
          (desc (read-string "Description: " choice)))
     (org-insert-link buffer-file-name (concat "*" choice) desc)))
 
+(use-package hammy
+  :load-path "packages/hammy.el/" "packages/svg-lib" "packages/ts.el"
+  :commands hammy-start
+  :hook (hammy-start . hammy-mode)
+  :config
+  ;; https://github.com/alphapapa/hammy.el/issues/10
+  ;; Use alert instead of notifications-notify, which do not work on MacOS platform.
+  (define-advice notifications-notify
+      (:override (&rest params) using-alert)
+    (alert (plist-get params :body)
+           :title (plist-get params :title)))
+
+  (hammy-define "Pomodoro"
+    :documentation "The classic pomodoro timer."
+    :intervals
+    (list
+     (interval :name "Work"
+               :duration "25 minutes"
+               :before (do (announce "Starting work time.")
+                           (notify "Starting work time."))
+               :advance (do (announce "Break time!")
+                            (notify "Break time!")))
+     (interval :name "Break"
+               :duration (do (if (and (not (zerop cycles))
+                                      (zerop (mod cycles 3)))
+                                 ;; If a multiple of three cycles have
+                                 ;; elapsed, the fourth work period was
+                                 ;; just completed, so take a longer break.
+                                 "30 minutes"
+                               "5 minutes"))
+               :before (do (announce "Starting break time.")
+                           (notify "Starting break time."))
+               :advance (do (announce "Break time is over!")
+                            (notify "Break time is over!"))))))
+
 (provide 'init-org+)
 ;;; init-org+.el ends here.
