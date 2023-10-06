04e3abe25983d3a7f0501996cd9a7449c2195161
Jousimies <duan_n@outlook.com>
2023-10-07 00:30:54 +0800

    popper and workgroup

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -234,5 +234,90 @@ of the box `(w h)' inside the box `(cw ch)'."
   :load-path "packages/ace-window"
   :bind ("M-o" . ace-window))
 
+(use-package workgroups2
+  :load-path "packages/workgroups2/src"
+  :hook (after-init . workgroups-mode)
+  :init
+  (setq wg-prefix-key "C-c p"))
+
+(use-package popper
+  :load-path "packages/popper/"
+  :bind (:map popper-mode-map
+              ("M-<tab>"   . popper-cycle)
+              ("M-`" . popper-toggle-type))
+  :hook (emacs-startup . popper-mode)
+  :init
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"
+          "Output\\*$" "\\*Pp Eval Output\\*$"
+          "\\*Compile-Log\\*"
+          "\\*Completions\\*"
+          "\\*Warnings\\*"
+          "\\*Async Shell Command\\*"
+          "\\*Apropos\\*"
+          "\\*Backtrace\\*"
+          "\\*Calendar\\*"
+          "\\*Embark Actions\\*"
+          "\\*Finder\\*"
+          "\\*Kill Ring\\*"
+          "\\*Go-Translate\\*"
+
+          bookmark-bmenu-mode
+          comint-mode
+          compilation-mode
+          help-mode helpful-mode
+          tabulated-list-mode
+          Buffer-menu-mode
+
+          gnus-article-mode devdocs-mode
+          grep-mode occur-mode rg-mode ag-mode pt-mode
+          youdao-dictionary-mode osx-dictionary-mode fanyi-mode
+
+          "^\\*Process List\\*" process-menu-mode
+          list-environment-mode cargo-process-mode
+
+          "^\\*eshell.*\\*.*$" eshell-mode
+          "^\\*shell.*\\*.*$"  shell-mode
+          "^\\*terminal.*\\*.*$" term-mode
+          "^\\*vterm.*\\*.*$"  vterm-mode
+          "^\\*eldoc.*\\*.*$" eldoc-mode
+
+          "\\*Flycheck errors\\*$" " \\*Flycheck checker\\*$"
+          "\\*Paradox Report\\*$" "\\*package update results\\*$" "\\*Package-Lint\\*$"
+          "\\*[Wo]*Man.*\\*$"
+          "\\*ert\\*$" overseer-buffer-mode
+          "\\*gud-debug\\*$"
+          "\\*lsp-help\\*$" "\\*lsp session\\*$"
+          "\\*quickrun\\*$"
+          "\\*tldr\\*$"
+          "\\*vc-.*\\*$"
+          "^\\*elfeed-entry\\*$"
+          "^\\*macro expansion\\**"
+
+          "\\*Agenda Commands\\*" "\\*Org Select\\*" "\\*Capture\\*" "^CAPTURE-.*\\.org*"))
+  :config
+  ;; mode-line indicator
+  ;; (with-eval-after-load 'popper
+  ;;   (setq popper-mode-line
+  ;;         '(:eval `(:propertize " POP |"
+  ;;                               face ,(if (+mode-line-window-active-p)
+  ;;                                         (+mode-line-get-window-name-face)
+  ;;                                       '+mode-line-meta-inactive-face)))))
+
+  ;; Enable indicator in minibuffer
+  ;; (popper-echo-mode 1)
+
+  ;; HACK: close popper with `C-g'
+  (defun +popper-close-window-hack (&rest _)
+    "Close popper window via `C-g'."
+    (when (and (called-interactively-p 'interactive)
+               (not (region-active-p))
+               popper-open-popup-alist)
+      (let ((window (caar popper-open-popup-alist)))
+        (when (window-live-p window)
+          (delete-window window)))))
+  (advice-add #'keyboard-quit :before #'+popper-close-window-hack)
+  )
+
 (provide 'init-ui)
 ;;; init-ui.el ends here.
