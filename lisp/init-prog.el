e3fd82974b674cc71c46fc39e377e28e64285047
Jousimies <duan_n@outlook.com>
2023-12-03 18:12:20 +0800

    prog configuration

---
+++ lisp/init-prog.el
@@ -0,0 +1,35 @@
+;;; init-prog.el --- Emacs Programming               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Duan Ning
+
+;; Author: Duan Ning <duan_n@outlook.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(use-package swift-mode
+  :load-path "packages/swift-mode/"
+  :mode ("\\.swift\\'" . swift-mode))
+
+(use-package yaml-ts-mode
+  :mode ("\\.yaml\\|\\.yml\\'" . yaml-ts-mode))
+
+(provide 'init-prog)
+;;; init-prog.el ends here
