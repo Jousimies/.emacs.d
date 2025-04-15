0d528499f0db65ae004079b49708ac76058bc8bb
DN <duan_n@outlook.com>
2025-04-15 16:04:31 +0800

    use ledger instead of finance.

--- lisp/init-finance.el
+++ lisp/init-finance.el
@@ -4,87 +4,96 @@
 
 ;;; Code:
 
-(use-package beancount
-  :load-path "packages/beancount-mode/"
-  :mode ("\\.bean\\'" . beancount-mode)
-  :hook ((beancount-mode . (lambda ()
-                             (setq-local electric-indent-chars nil)))
-         (beancount-mode . outline-minor-mode))
-  :config
-  (setq beancount-highlight-transaction-at-point t)
-  ;; insert whole transaction instead of only insert date.
-  ;; (defun my/beancount-insert-transaction (&optional days)
-  ;;   "Start a new timestamped directive with date shifted by DAYS from today."
-  ;;   (interactive "P")
-  ;;   (unless (bolp) (newline))
-  ;;   (insert (beancount--shift-current-date days) " * \"\" \"\"")
-  ;;   (backward-char 4))
+(use-package ledger-mode
+  :load-path "packages/ledger-mode/"
+  :mode ("\\.ledger\\'" . ledger-mode))
 
-  ;; (advice-add 'beancount-insert-date :override 'my/beancount-insert-transaction)
+;; (use-package beancount
+;;   :load-path "packages/beancount-mode/"
+;;   :mode ("\\.bean\\'" . beancount-mode)
+;;   :hook ((beancount-mode . (lambda ()
+;;                              (setq-local electric-indent-chars nil)))
+;;          (beancount-mode . outline-minor-mode))
+;;   :config
+;;   (setq beancount-highlight-transaction-at-point t)
+;;   ;; insert whole transaction instead of only insert date.
+;;   ;; (defun my/beancount-insert-transaction (&optional days)
+;;   ;;   "Start a new timestamped directive with date shifted by DAYS from today."
+;;   ;;   (interactive "P")
+;;   ;;   (unless (bolp) (newline))
+;;   ;;   (insert (beancount--shift-current-date days) " * \"\" \"\"")
+;;   ;;   (backward-char 4))
 
-  ;; Auto open browser after beancount-fava started.
-  (defun my/browser-beancount-fava ()
-    (if beancount--fava-process
-        (browse-url "http://127.0.0.1:5000")))
+;;   ;; (advice-add 'beancount-insert-date :override 'my/beancount-insert-transaction)
 
-  (advice-add 'beancount-fava :after 'my/browser-beancount-fava)
+;;   ;; Auto open browser after beancount-fava started.
+;;   (defun my/browser-beancount-fava ()
+;;     (if beancount--fava-process
+;;         (browse-url "http://127.0.0.1:5000")))
 
-  ;; auto align transaction before save file.
-  (defun my/beancount-align-transaction ()
-    "Align visible region in current buffer."
-    (save-excursion
-      (indent-region (window-start) (window-end))))
+;;   (advice-add 'beancount-fava :after 'my/browser-beancount-fava)
 
-  (add-hook 'before-save-hook (lambda ()
-                                (if (eq major-mode 'beancount-mode)
-                                    (my/beancount-align-transaction)))))
+;;   ;; auto align transaction before save file.
+;;   (defun my/beancount-align-transaction ()
+;;     "Align visible region in current buffer."
+;;     (save-excursion
+;;       (indent-region (window-start) (window-end))))
+
+;;   (add-hook 'before-save-hook (lambda ()
+;;                                 (if (eq major-mode 'beancount-mode)
+;;                                     (my/beancount-align-transaction)))))
 
 ;; Use double-entry-genertor generate beancount file from source date, provided by Alipay and Wechat.
-(defvar my/finance (expand-file-name "finance" my-galaxy))
-(defvar my/finance-source-data (expand-file-name "SourceData/" my/finance))
-(defvar DEG/config-dir (expand-file-name "Config/" my/finance))
-(defvar my/beancount-file (expand-file-name "AllBeans/" my/finance))
-(defvar DEG-cli "/opt/homebrew/bin/double-entry-generator")
+;; (defvar my/finance (expand-file-name "finance" my-galaxy))
+;; (defvar my/finance-source-data (expand-file-name "SourceData/" my/finance))
+;; (defvar DEG/config-dir (expand-file-name "Config/" my/finance))
+;; (defvar my/beancount-file (expand-file-name "AllBeans/" my/finance))
+;; (defvar DEG-cli "/opt/homebrew/bin/double-entry-generator")
 
-(defvar my/bean-regexp "\\([0-9]\\{8\\}[_-]\\([0-9]\\{6\\}\\|[0-9]\\{8\\}\\)\\)")
+;; (defvar my/bean-regexp "\\([0-9]\\{8\\}[_-]\\([0-9]\\{6\\}\\|[0-9]\\{8\\}\\)\\)")
 
-(defun my/bean-rename-source (file)
-  (when (string-match my/bean-regexp file)
-    (let ((new-file-path (concat my/finance-source-data "wechat_" (match-string 1 file) ".csv")))
-      (rename-file file new-file-path)
-      new-file-path)))
+;; (defun my/bean-rename-source (file)
+;;   (when (string-match my/bean-regexp file)
+;;     (let ((new-file-path (concat my/finance-source-data "wechat_" (match-string 1 file) ".csv")))
+;;       (rename-file file new-file-path)
+;;       new-file-path)))
 
 ;; According account data get from alipy and wechat, the csv file name has similiar pattern.
 ;; Generaly, I get data at the first day of month, the csv file will as `20240101' or `20240201'.
-(defvar finance-source-regexp (concat (format-time-string "%Y") "0[1-2]01"))
+;; (defvar finance-source-regexp (concat (format-time-string "%Y") "0[1-2]01"))
 
-(defun my/bean-generate (file)
-  (interactive (list (read-file-name "CSV transaction:"
-                                     my/finance-source-data nil nil finance-source-regexp)))
-  (let* ((file (if (string-match-p "alipay" file) file (my/bean-rename-source file)))
-		 (prefix (if (string-match-p "alipay" file) "alipay" "wechat"))
-		 (config (concat DEG/config-dir prefix ".yaml"))
-		 (output (concat my/beancount-file (file-name-base file) ".bean"))
-		 (provider (if (string= prefix "alipay") "" "--provider wechat")))
-	(shell-command (format "%s translate --config %s %s --output %s %s"
-                           DEG-cli config provider output file))))
+;; (defun my/bean-generate (file)
+;;   (interactive (list (read-file-name "CSV transaction:"
+;;                                      my/finance-source-data nil nil finance-source-regexp)))
+;;   (let* ((file (if (string-match-p "alipay" file) file (my/bean-rename-source file)))
+;; 		 (prefix (if (string-match-p "alipay" file) "alipay" "wechat"))
+;; 		 (config (concat DEG/config-dir prefix ".yaml"))
+;; 		 (output (concat my/beancount-file (file-name-base file) ".bean"))
+;; 		 (provider (if (string= prefix "alipay") "" "--provider wechat")))
+;; 	(shell-command (format "%s translate --config %s %s --output %s %s"
+;;                            DEG-cli config provider output file))))
 
 ;;;###autoload
-(defun my/beancount-fava ()
-  "Start (and open) or stop the fava server."
-  (interactive)
-  (require 'beancount)
-  (if beancount--fava-process
-      (progn
-        (delete-process beancount--fava-process)
-        (setq beancount--fava-process nil)
-        (message "Fava process killed"))
-    (setq beancount--fava-process
-          (start-process "fava" (get-buffer-create "*fava*") "fava"
-                         (if (eq 'beancount-mode major-mode) (buffer-file-name)
-                           (read-file-name "File to load: " my/finance nil nil nil))))
-    (set-process-filter beancount--fava-process #'beancount--fava-filter)
-    (message "Fava process started")))
+;; (defun my/beancount-fava ()
+;;   "Start (and open) or stop the fava server."
+;;   (interactive)
+;;   (require 'beancount)
+;;   (if beancount--fava-process
+;;       (progn
+;;         (delete-process beancount--fava-process)
+;;         (setq beancount--fava-process nil)
+;;         (message "Fava process killed"))
+;;     (setq beancount--fava-process
+;;           (start-process "fava" (get-buffer-create "*fava*") "fava"
+;;                          (if (eq 'beancount-mode major-mode) (buffer-file-name)
+;;                            (read-file-name "File to load: " my/finance nil nil nil))))
+;;     (set-process-filter beancount--fava-process #'beancount--fava-filter)
+;;     (message "Fava process started")))
+
+;; (use-package gnuplot
+;;   :load-path "~/.emacs.d/packages/gnuplot/"
+;;   :mode ("\\.gp$" . gnuplot-mode)
+;;   :commands gnuplot-context-sensitive-mode gnuplot-mode gnuplot-make-buffer)
 
 (provide 'init-finance)
 ;;; init-finance.el ends here.
