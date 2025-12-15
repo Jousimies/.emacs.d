b8b0b4ab4f12ece3bf00588d46a092c1c8653557
DN <duan_n@outlook.com>
2025-12-15 22:33:49 +0800

    init

---
+++ custom.el
@@ -0,0 +1,27 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(listen org-gtd persistent-cached-load-filter))
+ '(package-vc-selected-packages
+   '((reader :url "https://codeberg.org/divyaranjan/emacs-reader" :make "all")
+     (persistent-cached-load-filter :url
+				    "https://github.com/include-yy/persistent-cached-load-filter"
+				    :branch "main")
+     (eee :url "https://github.com/eval-exec/eee.el")
+     (ibooks-annot :url "https://github.com/Jousimies/ibooks-annot.el")
+     (corg :url "https://github.com/isamert/corg.el")
+     (emt :url "https://github.com/roife/emt")
+     (macos :url "https://github.com/xenodium/EmacsMacOSModule")
+     (go-translate :url "https://github.com/lorniu/gt.el")
+     (sdcv :url "https://github.com/manateelazycat/sdcv")
+     (rime-regexp :url "https://github.com/colawithsauce/rime-regexp.el")
+     (macim :url "https://github.com/roife/macim.el"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
