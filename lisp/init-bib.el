64a19a92c852b053d5c3692ea26b90ace1003689
Desktop <duan_n@outlook.com>
2026-08-17 21:55:04 +0800

    bibtex and reftex: remove use-package
    do not use use-package with builtin packages

--- lisp/init-bib.el
+++ lisp/init-bib.el
@@ -3,37 +3,33 @@
 (setopt my/reference-lists `(,(concat my-galaxy "/bibtexs/My Library.bib")
                              ,(concat my-galaxy "/bibtexs/Books.bib")
 			     ,(concat my-galaxy "/bibtexs/Seismic.bib")))
-(use-package bibtex
-  :ensure nil
-  :mode ("\\.bib\\'" . bibtex-mode)
-  :custom
-  (bibtex-align-at-equal-sign t)
-  (bibtex-autokey-year-length 4)
-  (bibtex-autokey-name-year-separator "-")
-  (bibtex-autokey-year-title-separator "-")
-  (bibtex-autokey-titleword-separator "-")
-  (bibtex-autokey-titlewords 2)
-  (bibtex-autokey-titlewords-stretch 1)
-  (bibtex-autokey-titleword-length 5))
 
-(use-package reftex
-  :ensure nil
-  :hook (LaTeX-mode . turn-on-reftex)
-  :bind ([remap reftex-citation] . citar-insert-citation)
-  :config
-  (setf (alist-get "\\*RefTex" display-buffer-alist nil t #'equal)
+(with-eval-after-load 'bibtex
+  (setopt bibtex-align-at-equal-sign t
+	  bibtex-autokey-year-length 4
+	  bibtex-autokey-name-year-separator "-"
+	  bibtex-autokey-year-title-separator "-"
+	  bibtex-autokey-titleword-separator "-"
+	  bibtex-autokey-titlewords 2
+	  bibtex-autokey-titlewords-stretch 1
+	  bibtex-autokey-titleword-length 5))
+
+(add-hook 'LaTeX-mode-hook #'tuan-on-reftex)
+(keymap-set global-map "<remap> <reftex-citation>" #'citar-insert-citation)
+(setf (alist-get "\\*RefTex" display-buffer-alist nil t #'equal)
         '((display-buffer-in-side-window)
           (window-height . 0.25)
           (side . bottom) (slot . -9)))
-  :custom
-  (reftex-insert-label-flags '("sf" "sfte"))
-  (reftex-plug-into-AUCTeX t)
-  (reftex-ref-style-default-list '("Default" "AMSmath" "Cleveref"))
-  (reftex-use-multiple-selection-buffers t)
-  (reftex-default-bibliography org-cite-global-bibliography)
-  (reftex-toc-follow-mode t)
-  (reftex-toc-split-windows-horizontally t)
-  (reftex-toc-split-windows-fraction 0.25))
+
+(with-eval-after-load 'reftex
+  (setopt reftex-insert-label-flags '("sf" "sfte")
+	  reftex-plug-into-AUCTeX t
+	  reftex-ref-style-default-list '("Default" "AMSmath" "Cleveref")
+	  reftex-use-multiple-selection-buffers t
+	  reftex-default-bibliography org-cite-global-bibliography
+	  reftex-toc-follow-mode t
+	  reftex-toc-split-windows-horizontally t
+	  reftex-toc-split-windows-fraction 0.25))
 
 (with-eval-after-load 'oc
   (setq org-cite-global-bibliography my/reference-lists))
