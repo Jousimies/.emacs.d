ba4e63ed0b3f26f9c0bbe4734264a3e2957f95c4
JGS <duan_n@outlook.com>
2026-08-19 13:29:54 +0800

    delete coding related configuration
    目前的设置总有不好使的地方，让 Emacs 自己决定吧。

--- lisp/init-builtin.el
+++ lisp/init-builtin.el
@@ -1,20 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-;; Coding system
-(set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-;; (set-selection-coding-system 'utf-8)
-
-;; (setq default-process-coding-system '(utf-8 . utf-8))
-;; (setq file-name-coding-system 'utf-8)
-;; (setq selection-coding-system 'utf-8)
-;; (setq-default buffer-file-coding-system 'utf-8)
-(when (fboundp 'w32-set-console-codepage)
-  (w32-set-console-codepage 65001))
-
 (setq use-short-answers t)
 (setq server-client-instructions nil)
 ;;关闭 ring bell,用 mode-line 替代
